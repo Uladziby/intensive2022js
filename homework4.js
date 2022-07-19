299ff1c9cf2b2b4c3bdb0342aa4b1b90882ca218
Vlad Yanushevsky <yanushevskyv@gmail.com>
2022-07-19 23:33:17 +0200

    add homework 4 tasks 1 2

--- homework4.js
+++ homework4.js
@@ -11,3 +11,44 @@ function concatStrings(str, separator = null) {
     }
     return concat(str)
 }
+class Calculator {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        if (!this.isValid(this.x) || !this.isValid(this.y)) throw new Error('not valid')
+    }
+    isValid(number) {
+        return typeof number === 'number' && isFinite(number) && !isNaN(number) && typeof number !== 'bigint' ? true : false;
+    }
+    setX(number) {
+        if (this.isValid(number)) {
+            this.x = number;
+        } else throw new Error('number must not be a bigint')
+    }
+    setY(number) {
+        if (this.isValid(number)) {
+            this.y = number;
+        }else throw new Error('number must not be a bigint')
+    }
+    logSum() {
+        console.log(this.x + this.y);
+    }
+    logDiv() {
+        console.log(this.y, this.x)
+        if (this.y === 0) throw new Error('Y cant be equal zero')
+        console.log(this.x / this.y);
+    }
+    logSub() {
+        console.log(this.x - this.y);
+    }
+    logMul() {
+        console.log(this.x * this.y);
+    }
+}
+/* const calculator = new Calculator(12, 3);
+calculator.logSum(); // 15
+calculator.logDiv(); // 4
+calculator.setX(15);
+const logCalculatorDiv = calculator.logDiv;
+calculator.setY(444n); // Ошибка!
+calculator.logDiv(); // 5 */
